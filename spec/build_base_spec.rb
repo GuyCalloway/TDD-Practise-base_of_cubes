ee7fba92a1b9e37cccd0df32e6552d69d06db285
GuyCalloway <guyfcalloway@gmail.com>
2019-10-23 18:21:04 +0100

    Last two tests passing

--- spec/build_base_spec.rb
+++ spec/build_base_spec.rb
@@ -2,7 +2,7 @@ require 'build_base.rb'
 
 describe BaseBuild do
     buildBase = BaseBuild.new
-    
+
     it "returns 1 cube for volume 1" do
         expect(buildBase.calc(1)).to eq(1)
     end
@@ -25,5 +25,13 @@ describe BaseBuild do
 
     it "returns 3 cube for volume 36" do
         expect(buildBase.calc(36)).to eq(3)
+    end
+
+    it "returns -1 cube for volume 91716553919377" do
+        expect(buildBase.calc(91716553919377)).to eq(-1)
+    end
+
+    it "returns 45 cube for volume 1071225" do
+        expect(buildBase.calc(1071225)).to eq(45)
     end
 end
